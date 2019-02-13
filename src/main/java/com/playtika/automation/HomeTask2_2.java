270493a89bce1844429e7e924a38fc24d7f8df61
akhusainov <akhusainov@playtika.com>
2019-02-13 15:37:05 +0200

    Implement HomeTask2_1

---
+++ src/main/java/com/playtika/automation/HomeTask2_2.java
@@ -0,0 +1,23 @@
+package com.playtika.automation;
+
+import java.util.Scanner;
+
+public class HomeTask2_2 {
+    public static void main(String[] args) {
+        Scanner console = new Scanner(System.in);
+        System.out.println("Please input number to receive sum of its digits and press Enter key: ");
+        long number = console.nextLong();
+
+        sumDigits(number);
+    }
+
+    public static void sumDigits(long number) {
+        long sum = 0;
+        while (number != 0) {
+            sum = (number % 10) + sum;
+            number = number / 10;
+        }
+        System.out.println(Math.abs(sum));
+
+    }
+}
