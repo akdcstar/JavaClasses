3d8cc68415a402c6b23407af7d3004336f52b4d2
akhusainov <akhusainov@playtika.com>
2019-02-15 11:12:41 +0200

    Implement HomeTask3_1

--- src/main/java/com/playtika/automation/HomeTasks/Second/HomeTask2_3.java
+++ src/main/java/com/playtika/automation/HomeTasks/Second/HomeTask2_3.java
@@ -1,6 +1,5 @@
 package com.playtika.automation.HomeTasks.Second;
 
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class HomeTask2_3 {
@@ -9,20 +8,25 @@ public class HomeTask2_3 {
         System.out.println("Please input number of developers: ");
         int numberOfDevelopers = console.nextInt();
 
-    sumOfInterview(numberOfDevelopers);
+        sumOfInterview(numberOfDevelopers);
+
+
     }
 
     public static void sumOfInterview(int numberOfDevelopers) {
-//        int[] interviewArray = new int [numberOfDevelopers];
-//        for (int i = numberOfDevelopers; i >= 1; i--){
-//            int interviewCount = i-1;
-//            System.out.println(interviewCount);
-//            interviewArray[i-1] = interviewCount;
-//            System.out.println(Arrays.;
+        int sum = 0;
+        for (int i = 1; i <= numberOfDevelopers; i++) {
+
+            int interviewCount = i - 1;
+            System.out.println(interviewCount);
+            sum = sum + interviewCount;
+        }
+/*      implementation in one line using formula
         int sum = numberOfDevelopers * (numberOfDevelopers - 1) / 2;
+*/
         System.out.println(sum);
 
-        }
+    }
 
 }
 
