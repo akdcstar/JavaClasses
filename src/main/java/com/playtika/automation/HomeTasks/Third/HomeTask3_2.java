f5895fc109b402602a14cb30d2f5da145f3e2f98
akhusainov <akhusainov@playtika.com>
2019-02-15 12:48:04 +0200

    Implement HomeTask3_2

---
+++ src/main/java/com/playtika/automation/HomeTasks/Third/HomeTask3_2.java
@@ -0,0 +1,28 @@
+package com.playtika.automation.HomeTasks.Third;
+
+import java.util.Scanner;
+
+public class HomeTask3_2 {
+    public static void main(String[] args) {
+        Scanner console = new Scanner(System.in);
+        System.out.println("Please input sequence of characters with spaces: ");
+        String chars = console.nextLine();
+
+        reverseWordsInLine(chars);
+    }
+
+    public static void reverseWordsInLine(String chars) {
+
+        String[] splittedChars = chars.split(" ");
+
+        for (int i = 0; i < splittedChars.length; i++) {
+            StringBuilder reversedChars = new StringBuilder(splittedChars[i]);
+            String result = reversedChars.reverse().toString();
+            System.out.print(result);
+            if (i < (splittedChars.length) - 1) {
+                System.out.print(" ");
+            }
+        }
+
+    }
+}
