05155d31398490e7655c1633b1d67658c473387f
DC_star <akdcstar@gmail.com>
2019-02-25 23:02:22 +0200

    Implementation of inherited classes and shapes area calculation

---
+++ src/main/java/com/playtika/automation/Courses/InheritedClasses.java
@@ -0,0 +1,54 @@
+package com.playtika.automation.Courses;
+
+
+//Реализовать родительский класс геометрической фигуры и наследуемые от него классы.
+//В каждом классе есть методы, которые считают площадь фигуры
+
+import java.awt.*;
+import java.sql.SQLOutput;
+
+public class InheritedClasses {
+    public static void main(String[] args) {
+
+
+        class Figure {
+            double pi = Math.PI;
+            double mainSide;
+
+        }
+
+        class Circle extends Figure {
+            public double circleArea(double radius) {
+                double area = pi * (Math.pow(radius, 2));
+                return area;
+
+            }
+
+        }
+
+        class Square extends Figure {
+            public double squareArea(double mainSide) {
+                double area = Math.pow(mainSide, 2);
+                return area;
+            }
+
+        }
+
+        class Rectangle extends Figure {
+            public double rectangleArea(double mainSide, double additionalSide) {
+                double area = mainSide * additionalSide;
+                return area;
+            }
+        }
+
+        Circle C = new Circle();
+        System.out.println("Площадт круга: " + C.circleArea(10));
+
+        Square S = new Square();
+        System.out.println("Площадь квадрата: " + S.squareArea(20));
+
+        Rectangle R = new Rectangle();
+        System.out.println("Площадь прямоугольника: " + R.rectangleArea(30,40));
+
+    }
+}
