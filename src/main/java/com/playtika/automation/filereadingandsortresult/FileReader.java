923cfc3dddcd46e37568f907201bc11e03b53ca8
akhusainov <akhusainov@playtika.com>
2019-03-29 19:16:06 +0200

    Treemap

--- src/main/java/com/playtika/automation/filereadingandsortresult/FileReader.java
+++ src/main/java/com/playtika/automation/filereadingandsortresult/FileReader.java
@@ -1,18 +1,30 @@
 package com.playtika.automation.filereadingandsortresult;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.TreeMap;
+
 public class FileReader {
 
-    public void read(String filePath){
-//        System.out.println(filePath);
-//        ArrayList textFromFile = new ArrayList();
-//
-//        try (Scanner scanFile = new Scanner(new File(filePath))) {
-//            ArrayList textFromFile = new ArrayList();
-//            while (scanFile.hasNext())
-//            textFromFile.add(scanFile.next());
-//        } catch (FileNotFoundException e) {
-//            e.printStackTrace();
-//        }System.out.println(Arrays.toString(textFromFile));
-//            System.out.println(" ");
+    public void read(String filePath) {
+        System.out.println(filePath);
+        TreeMap textFromFile = new TreeMap();
+
+        try (Scanner scanFile = new Scanner(new File(filePath))) {
+            while (scanFile.hasNext())
+
+                if(textFromFile.containsKey(scanFile.next())){
+                    textFromFile.put(scanFile.next(), n + 1);
+                }
+                textFromFile.put(scanFile.next(), );
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        for (int i = 0; 1 < textFromFile.size(); i++) {
+            System.out.println(textFromFile.get(i));
+        }
     }
 }
