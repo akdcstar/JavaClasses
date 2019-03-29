b95cc91273e549ac4a260895dba0457a026602d3
akhusainov <akhusainov@playtika.com>
2019-03-29 15:07:36 +0200

    Match pattern from file

--- src/main/java/com/playtika/automation/Grep/ReadFile.java
+++ src/main/java/com/playtika/automation/Grep/ReadFile.java
@@ -3,23 +3,28 @@ package com.playtika.automation.Grep;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class ReadFile {
 
-    public static void read(String filePath, String line) {
-        try {
-            File file = new File(filePath);
-            Scanner scanFile = new Scanner(file);
+    public void read(String filePath, String pattern) {
+        try (Scanner scanFile = new Scanner(new File(filePath))) {
             int counter = 0;
+
+            Pattern linePattern = Pattern.compile(pattern);
+
             while (scanFile.hasNextLine()) {
                 String fileText = scanFile.nextLine();
                 counter += 1;
-                if (fileText.toLowerCase().contains(line)) {
+                Matcher lineMatcher = linePattern.matcher(fileText.toLowerCase());
+                if (lineMatcher.find()) {
 
                     System.out.println("Line: " + counter + ".  " + fileText);
                 }
+                System.out.println("There is no any match in file with your pattern");
             }
-            scanFile.close();
+
         } catch (FileNotFoundException e) {
             System.out.println("File is not found");
         }
--- src/main/java/com/playtika/automation/Grep/Runner.java
+++ src/main/java/com/playtika/automation/Grep/Runner.java
@@ -1,11 +1,19 @@
 package com.playtika.automation.Grep;
 
+import java.util.Scanner;
+
 public class Runner {
     public static void main(String[] args) {
-       String filePath = "C:\\Users\\akhusainov\\_DC\\_Docs\\JavaTestFile.txt";
-       String comparedLine = "slot";
 
-       ReadFile fileFromConsole = new ReadFile();
-       fileFromConsole.read(filePath, comparedLine.toLowerCase());
+        Scanner consoleFilePath = new Scanner(System.in);
+        System.out.println("Please input file path (e.g. 'C:\\Users\\akhusainov\\_DC\\_Docs\\JavaTestFile.txt): '");
+        String filePath = consoleFilePath.nextLine();
+
+        Scanner consolePattern = new Scanner(System.in);
+        System.out.println("Please input pattern which you would like to check (e.g. ([a-z]{10,10}) or 'slot'): ");
+        String pattern = consolePattern.nextLine();
+
+        ReadFile fileFromConsole = new ReadFile();
+        fileFromConsole.read(filePath, pattern.toLowerCase());
     }
 }
